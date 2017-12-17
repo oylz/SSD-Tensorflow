19882c4e46506e11a147b091b5077e8d27b96a2f
oylz <oylzxyz@gmail.com>
2017-12-17 22:55:47 +0800

    add convert run shell (r.sh)

---
+++ r.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ./chgpath
+
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0-cudnn6.0/lib64/:$LD_LIBRARY_PATH
+echo $LD_LIBRARY_PATH
+
+export LD_LIBRARY_PATH=/home/xyz/code1/caffe-ssd/distribute/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/home/xyz/code1/caffe-ssd/distribute/python:$PYTHONPATH
+echo "PYTHONPATH:"$PYTHONPATH
+
+
+
+
+python caffe_to_tensorflow.py \
+--model_name=ssd_300_vgg \
+--num_classes=21 \
+--caffemodel_path=ssddeploys/4_300/VGG_coco_SSD_300x300.caffemodel
+
+
+
+
